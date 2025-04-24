06d8e6d63a2b4079f275b0c9e044949a523d6aeb
Joel Eberhardsson <joeeb477@student.liu.se>
2025-04-24 15:42:23 +0200

    Fifth commit

--- Labb2/Labb2/main.asm
+++ Labb2/Labb2/main.asm
@@ -3,18 +3,20 @@
 ;
 
 .equ	T=10
-.equ	N=10
+.equ	N=70
 
 ldi		r16,HIGH(RAMEND)
 out		SPH,r16
 ldi		r16,LOW(RAMEND)
 out		SPL,r16
-ldi		r16,0b00000001
-out		DDRB,r16	; Sätter PB1 till utsignal
+ldi		r16,0b10000000
+out		DDRB,r16
 clr		r16
 
+jmp		AGAIN 
+
 MESSAGE:	; Sparar ordet i flash
-	.db		"DATORTEKNIK",$00	
+	.db		"O O",$00
 BTAB:		; Sparar binärkodningen i flash
 	.db		$60,$88,$A8,$90,$40,$28,$D0,$08,$20,$78,$B0,$48,$E0,$A0,$F0,$68,$D8,$50,$10,$C0,$30,$18,$70,$98,$B8,$C8
 
@@ -26,6 +28,8 @@ AGAIN:
 ;;;;;;;;;;;;;;;;;;;; Subrutin DELAY(r16=length)
 DELAY:
 	push	r17
+	push	r18
+	ldi		r18,T
 DELAY_OUTER_LOOP:
 	ldi		r17,$1F
 DELAY_INNER_LOOP:
@@ -33,6 +37,7 @@ DELAY_INNER_LOOP:
 	brne	DELAY_INNER_LOOP
 	dec		r18
 	brne	DELAY_OUTER_LOOP
+	pop		r18
 	pop		r17
 	ret
 
@@ -43,24 +48,19 @@ HW_INIT_EXIT:
 	ret		; gå till MORSE
 
 GET_CHAR:
-	lpm		r16,Z+ ; Detta loopar inte så vi får bara in 'D', men det fungerar
-	;brne	LOOKUP
-	;call	SEND
-	;brne	GET_CHAR
+	lpm		r16,Z+
+	cpi		r16,' '
+	breq	DO_SPACE
 GET_CHAR_EXIT:
-	ret		; gå till MORSE
+	ret	
 
-SEND_IT:
-	call	BEEP_CHAR
-	brne	GET_CHAR
-SEND_IT_EXIT:
-	ret
+
 
 BEEP_CHAR:
 	call	LOOKUP
 	call	SEND
-	ldi		r17,2*N
-	;call	NOBEEP
+	ldi		r18,2*N
+	call	NOBEEP
 BEEP_CHAR_EXIT:
 	ret
 
@@ -68,10 +68,12 @@ SEND:
 	lsl		r16
 	ldi		r18,3*N
 SEND_BITS:
-	brcc	SHORT_BEEP	; Hur sätter vi längden?
+	brcc	SHORT_BEEP
 	brcs	BEEP
-	;call	NOBEEP
-	andi	r16,$FF
+SEND_BITS_2:
+	ldi		r18,N
+	call	NOBEEP
+	cpi		r16,0b10000000
 	brne	SEND
 SEND_EXIT:
 	ret
@@ -82,15 +84,26 @@ BEEP:
 	sbi		PORTB,7	
 	call	DELAY
 	cbi		PORTB,7
-BEEP_EXIT:
+	call	DELAY
+	dec		r18
+	brne	BEEP
+	jmp		SEND_BITS_2
+
+NOBEEP:
+	cbi		PORTB,7	
+	call	DELAY
+	cbi		PORTB,7
+	call	DELAY
+	dec		r18
+	brne	NOBEEP
+NOBEEP_EXIT:
 	ret
 
-;NOBEEP:
-;	ldi		r18,N
+SEND_IT:
+	call	BEEP_CHAR
+	jmp		MORSE_LOOP
 
-
-
-LOOKUP:
+LOOKUP: ; Konverterar till ASCII
 	push	ZH
 	push	ZL
 	ldi		ZH,HIGH(BTAB*2)
@@ -103,13 +116,24 @@ LOOKUP:
 LOOKUP_EXIT:
 	ret
 
-
+DO_SPACE:
+	ldi		r18,N
+	push	r16
+	ldi		r16,7
+DO_SPACE_LOOP:
+	call	NOBEEP
+	dec		r16
+	brne	DO_SPACE_LOOP
+	pop		r16
+	jmp GET_CHAR
 
 ;;;;;;;;;;;;;;;;;;;; Huvudprogrammet
 MORSE:
 	call	HW_INIT
+MORSE_LOOP:
 	call	GET_CHAR
-	call	SEND_IT
+	cpi		r16,$00
+	brne	SEND_IT
 MORSE_EXIT:
 	ret		; Gå till AGAIN
 
